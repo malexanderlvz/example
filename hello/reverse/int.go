d33f58e19ffe89577d30997585b41f64c4620d88
Marcos Chavez <marcos.chavez@freeagentsoftware.com>
2024-01-22 12:55:18 -0800

    feat: created int under reverse folder for go multi-module workspaces

---
+++ hello/reverse/int.go
@@ -0,0 +1,10 @@
+package reverse
+
+import "strconv"
+
+// Int revturns the decimal reversal of the integer i.
+func Int(i int) int {
+    i, _ = strconv.Atoi(String(strconv.Itoa(i)))
+    return i
+}
+
